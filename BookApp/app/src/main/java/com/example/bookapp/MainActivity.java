628fed689b53692c7e0d16cf2170a43a1ecfcb78
t8hami02 <t8hami02@students.oamk.fi>
2024-01-12 18:50:24 +0200

    Added some basic components like EditTexts on activities

--- BookApp/app/src/main/java/com/example/bookapp/MainActivity.java
+++ BookApp/app/src/main/java/com/example/bookapp/MainActivity.java
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private Button button2;
+    private EditText editTextName;
+    private EditText editTextPassword;
+
+    public String seeEditText;
+    public String testText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         button = (Button) findViewById(R.id.buttonLogin);
+        button2 = (Button) findViewById(R.id.buttonReg);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        button2 = (Button) findViewById(R.id.buttonReg);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,8 +48,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void LoginButtonPress(){
-        Intent intent = new Intent(this, UserMainActivity.class);
-        startActivity(intent);
+
+        seeEditText = editTextName.getText().toString();
+        //testText = editTextName.getText().toString();
+        Log.d("Test1", seeEditText);
+
+        if (editTextName.getText().toString().equals("aa")){
+            Intent intent = new Intent(this, UserMainActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Log.d("Test1", "fail");
+        }
     }
 
     public void RegButtonPress(){
